597f1c874e9dae72cb46fcc87cfba86774dfe214
didox2 <didox222@gmail.com>
2017-07-13 00:32:48 +0300

    Exception tasks added

---
+++ Module I/Exception-Handling/Homework/01. Square root/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01.Square_root
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            try
+            {
+                string input = Console.ReadLine();
+                double number = double.Parse(input);
+                if (number < 0)
+                {
+                    throw new Exception("Invalid number");
+                }
+                Console.WriteLine("{0:F3}",Math.Sqrt(number));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
---
+++ Module I/Exception-Handling/Homework/02. Enter numbers/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _02.Enter_numbers
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            ReadNumber(1, 100);
+        }
+
+        public static void ReadNumber(int start, int end)
+        {
+            int[] numbers = new int[10];
+            try
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    numbers[i] = int.Parse(Console.ReadLine());
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("Exception");
+            }
+            try
+            {
+                for (int i = 0; i < numbers.Length - 1; i++)
+                {
+                    if (numbers[i] <= start || numbers[i] >= end || numbers[i] >= numbers[i + 1])
+                    {
+                        throw new Exception("Exception");
+                    }
+                }
+                Console.WriteLine("1 < " + string.Join(" < ", numbers) + " < 100");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
